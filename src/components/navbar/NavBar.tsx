21b5769172eaf55191239cb9e54a27a2bb2078c1
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-13 18:04:41 -0500

    Created NavBar component

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,10 @@
+import '../../App.css'
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark fw-bold fs-1  bg-dark p-3 text-white">
+      <span className="navbar-brand mx-auto">Welcome to my Pokemon React App</span>
+    </nav>
+  )
+}
+
+export default NavBar
