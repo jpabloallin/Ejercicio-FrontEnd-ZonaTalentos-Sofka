1df41ab2d97c23324a37df8507cc057048c2fc4e
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-13 18:05:26 -0500

    Created Pokémon Dashboard

---
+++ src/pages/dashboard/PokemonDashboard.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+interface IProps {}
+
+const PokemonDashboard : React.FC<IProps> = () => {
+    return <>
+    Pokemon dashboard
+    </>
+}
+
+export default PokemonDashboard
