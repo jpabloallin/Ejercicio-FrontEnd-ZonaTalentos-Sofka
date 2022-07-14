a7d229fb261ba620544f586cb5e282b0b4bcbc2c
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-14 13:59:56 -0500

    Added input to search pokemon by name

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -7,6 +7,7 @@ import Card from 'react-bootstrap/Card';
 import PokemonInformation from "../../components/pokemon/PokemonInformation";
 import { Link } from "react-router-dom";
 import { CardGroup, Col, Container, Row } from "react-bootstrap";
+import { FaSearch } from 'react-icons/fa';
 
 interface IPokemonProps {
     id: string;
@@ -17,7 +18,17 @@ const Main : React.FC = () => {
 
     const [ pokemon, setPokemon ] = React.useState<IPokemonProps[]>([])
     const [pokemonSearch, setPokemonSearch] = React.useState('');
-    const [pokemonListLimit, setPokemonListLimit] = React.useState(POKEMON_BY_PAGE);
+
+    const handleSearchPokemon = React.useCallback(async () => {
+        const response = await apiUrl.get(`/pokemon?limit=${NUMBER_MAX_POKEMONS}`);
+    
+        setPokemonSearch(pokemonSearch.toLocaleLowerCase());
+        // Valida nomes dos pokémons constam no valor da variável pokemonSearch
+        const pokemonsSearch = response.data.results.filter(
+          ({ name }: IPokemonProps) => name.includes(pokemonSearch),
+        );
+        setPokemon(pokemonsSearch);
+      }, [pokemonSearch]);
 
     const pokemonList = React.useCallback(async () => {
         const res = await apiUrl.get('/pokemon', {
@@ -29,11 +40,25 @@ const Main : React.FC = () => {
     },[]);
 
     React.useEffect(() => {
-        pokemonList();
-    }, [pokemonList])
+        const isSearch = pokemonSearch.length >= 2;
+
+        if (isSearch) handleSearchPokemon();
+        else pokemonList();
+
+    }, [pokemonSearch, pokemonList, handleSearchPokemon])
 
     return <>
         <Container>
+        <br />
+            <Container>
+                <FaSearch className="mx-2"/>
+                <input
+                placeholder={'Search Pokémon'}
+                value={pokemonSearch}
+                onChange={e => setPokemonSearch(e.target.value)}
+                />
+            </Container>
+            <br />
                 <Row xm={1} md={5} className="g-4">
                     {pokemon.map((pokemon) => (
                         <Col>
