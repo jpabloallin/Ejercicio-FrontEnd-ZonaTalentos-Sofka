0a3e39abbcac5f35fa9a7b0a09c1c07471ce68f0
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-14 13:14:51 -0500

    Created petition to get 50 pokemons from the api to display them

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,50 @@
+import axios from "axios";
+import * as React from "react"
+import apiUrl from "../../features/global/apiUrl"
+import {POKEMON_BY_PAGE} from "../../features/global/pokemonNumber"
+import {NUMBER_MAX_POKEMONS} from "../../features/global/pokemonNumber"
+import Card from 'react-bootstrap/Card';
+import PokemonInformation from "../../components/pokemon/PokemonInformation";
+import { Link } from "react-router-dom";
+import { CardGroup, Col, Container, Row } from "react-bootstrap";
 
-const Main = () => {
-  return (
-    <div>
-        <h1> Bienvenidos a la página de inicio!</h1>
-    </div>
-  )
+interface IPokemonProps {
+    id: string;
+    name: string;
+}
+
+const Main : React.FC = () => {
+
+    const [ pokemon, setPokemon ] = React.useState<IPokemonProps[]>([])
+    const [pokemonSearch, setPokemonSearch] = React.useState('');
+    const [pokemonListLimit, setPokemonListLimit] = React.useState(POKEMON_BY_PAGE);
+
+    const pokemonList = React.useCallback(async () => {
+        const res = await apiUrl.get('/pokemon', {
+            params: {
+                limit: POKEMON_BY_PAGE,
+            },
+        });
+        setPokemon(res.data.results);
+    },[]);
+
+    React.useEffect(() => {
+        pokemonList();
+    }, [pokemonList])
+
+    return <>
+        <Container>
+                <Row xm={1} md={5} className="g-4">
+                    {pokemon.map((pokemon) => (
+                        <Col>
+                            <Card style={{ width: '13rem' }}>
+                                <PokemonInformation  name = {pokemon.name} />
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+        </Container>
+    </>
 }
 
 export default Main
