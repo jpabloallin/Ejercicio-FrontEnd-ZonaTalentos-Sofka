19741ac7302ad1b5219813a222ccf1b44b2c0544
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-15 11:58:54 -0500

    Shows LogOut button and owner name when a user is logged in

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,9 +1,31 @@
+import { useSelector } from 'react-redux'
 import '../../App.css'
+import { RootState } from '../../app/store'
+import LogOut from '../authentication/LogOut'
 const NavBar = () => {
-  return (
+
+  const{user} = useSelector((state:RootState) => state.logged)
+
+  return (<>
+    {user!==null? 
+      <nav className="navbar navbar-dark bg-dark fw-bold fs-1  bg-dark p-3 text-white">
+      <span className="navbar-brand mx-auto d-flex justify-content-center">
+        <p className="mx-3 mt-2">Welcome to my Pokemon React App</p>  
+        <img src="https://img.icons8.com/plasticine/50/000000/open-pokeball.png"/>
+      </span>
+      <span className="navbar-brand ">
+        <p className="h6 mt-3">By Juan Pablo Allin Cañas</p> 
+      </span>
+      <span><LogOut/></span>
+    </nav>:
     <nav className="navbar navbar-dark bg-dark fw-bold fs-1  bg-dark p-3 text-white">
-      <span className="navbar-brand mx-auto">Welcome to my Pokemon React App <img src="src\assets\favicon-pokeball.png" alt=""/></span>
+      <span className="navbar-brand mx-auto d-flex justify-content-center">
+        <p className="mx-3 mt-2">Welcome to my Pokemon React App</p>  
+        <img src="https://img.icons8.com/plasticine/50/000000/open-pokeball.png"/>
+      </span>
     </nav>
+    }
+    </>
   )
 }
 
