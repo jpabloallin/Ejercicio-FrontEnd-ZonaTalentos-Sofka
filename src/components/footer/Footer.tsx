1b9549024cc52f37624497bf5e434774bdbfdcd6
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-14 19:21:16 -0500

    Small modifications on footer and NavBar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import '../../App.css'
 
 const Footer = () => {
     return (
-      <footer className="text-center bg-dark text-white p-3">
+      <footer className="text-center bg-dark text-white p-1">
         <p>
           Powered by <b>Juan Pablo Allin Cañas</b>
         </p>
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -2,8 +2,7 @@ import '../../App.css'
 const NavBar = () => {
   return (
     <nav className="navbar navbar-dark bg-dark fw-bold fs-1  bg-dark p-3 text-white">
-      <span className="navbar-brand mx-auto">Welcome to my Pokemon React App <img src="src\assets\pokeball-image.png" alt=""/></span>
-      
+      <span className="navbar-brand mx-auto">Welcome to my Pokemon React App <img src="src\assets\favicon-pokeball.png" alt=""/></span>
     </nav>
   )
 }
