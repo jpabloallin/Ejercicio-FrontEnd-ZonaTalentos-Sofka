b791cd757a7f633e7035f7f4f156d4246f3081ba
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-15 11:56:55 -0500

    Added AuthenticationPage to the Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import NavBar from "../components/navbar/NavBar";
+import AuthenticatorPage from "../pages/authentication/AuthenticationPage";
 import Main from "../pages/main/Main";
 import SingularPokemon from "../pages/pokemon/SingularPokemon";
 
@@ -12,6 +13,7 @@ const AppRoutes:React.FC<IProps> = () => {
     return <BrowserRouter>
         <NavBar/>
         <Routes>
+            <Route path="/login" element={<AuthenticatorPage/>} />
             <Route path="/" element={<Main/>} />
             <Route path="/pokemon/:name" element={<SingularPokemon/>} />
         </Routes>
