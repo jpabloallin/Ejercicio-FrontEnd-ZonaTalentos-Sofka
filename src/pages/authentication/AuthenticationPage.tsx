27cc6fe371d09190ab53fe28e520bb08385d5a06
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2022-07-15 11:56:15 -0500

    Created authentication page

---
+++ src/pages/authentication/AuthenticationPage.tsx
@@ -0,0 +1,23 @@
+import GoogleLogIn from "../../components/authentication/GoogleLogin";
+import LogIn from "../../components/authentication/LogIn";
+import SignIn from "../../components/authentication/SingIn";
+
+const AuthenticatorPage = () => {
+
+    return (
+      <div>
+        <br />
+        <h3>Sign in/Log in to see the Pokémon universe!</h3>
+        <div className="d-flex justify-content-around">
+          <div className="p-4">
+            <SignIn/>
+          </div>
+          <div className="p-4">
+            <LogIn/>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AuthenticatorPage;
